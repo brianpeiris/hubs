e958322ce35b95a3e7eabe8e2121c02e89826b6e
Brian Peiris <brianpeiris@gmail.com>
2020-04-13 11:28:21 -0700

    front page browserstack test

---
+++ test/live/front-page.js
@@ -0,0 +1,55 @@
+const test = require("ava");
+const webdriver = require("selenium-webdriver");
+
+const browsers = {
+  firefox: { browserName: "Firefox", os: "Windows", os_version: "10" },
+  //chrome: { browserName: "Chrome", os: "Windows", os_version: "10" },
+  ie: { browserName: "IE", os: "Windows", os_version: "10" },
+  edge: { browserName: "Edge", os: "Windows", os_version: "10" },
+  //safari_mac: { browserName: "Safari", os: "OS X", os_version: "Catalina" },
+  safari_ios: { browserName: "iPhone", device: "iPhone 8", os_version: "13", real_mobile: true }
+};
+
+const creds = {
+  "browserstack.user": process.env.BROWSERSTACK_USER,
+  "browserstack.key": process.env.BROWSERSTACK_KEY
+};
+
+async function retryUntilElContains(driver, selector, expectedText, retry = 10) {
+  for (let i = 0; i < retry; i++) {
+    const els = await driver.findElements(webdriver.By.css(selector));
+
+    for (const el of els) {
+      const text = await el.getText();
+      if (text === expectedText) {
+        return;
+      }
+    }
+
+    await driver.sleep(200);
+  }
+
+  throw new Error("failed to find element", selector, expectedText);
+}
+
+for (const browser in browsers) {
+  const capabilities = Object.assign(browsers[browser], creds);
+
+  const driver = new webdriver.Builder()
+    .usingServer("http://hub-cloud.browserstack.com/wd/hub")
+    .withCapabilities(capabilities)
+    .build();
+
+  test(browser, async t => {
+    await driver.get("https://hubs.mozilla.com");
+
+    try {
+      await retryUntilElContains(driver, "button", "Create a Room");
+      t.pass("CTA button found");
+    } catch (e) {
+      t.fail("CTA button not found");
+    }
+
+    driver.quit();
+  });
+}
